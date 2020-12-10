e18c763eb6e4cdafd2454d798b86c7fab4e1a9f8
Erin Simshauser <erin_simshauser@brown.edu>
2020-12-10 18:43:21 -0500

    add back ycombo

--- sentiment.py
+++ sentiment.py
@@ -16,6 +16,40 @@ def index_2d(myList, v):
         if v in x:
             return (i, x.index(v))
 
+def proportion(filepath, row_num):
+    sent = list()
+    array = []
+
+    file =  open(filepath, 'r')
+    rows = csv.reader(file)
+    
+    # for row in rows:
+    #     array.append(row)
+    #     sentence = row[row_num]
+    #     blob = TextBlob(sentence)
+    #     sent.append(blob.sentiment.polarity)
+
+    sentiment_scores_tb = [round(TextBlob(row[row_num]).sentiment.polarity, 3) for row in rows]
+    sentiment_category_tb = ['positive' if score > 0 
+                             else 'negative' if score < 0 
+                                 else 'neutral' 
+                                     for score in sentiment_scores_tb]
+    pos=0
+    neg=0
+    neut=0
+    for item in sentiment_category_tb:
+        if item == 'positive':
+            pos+=1
+        elif item == 'negative':
+            neg+=1
+        elif item == 'neutral':
+            neut+=1
+
+    print(pos, neg, neut)
+
+#proportion('src/data/scraping_data/dep_def.csv', 0)
+
+
 def sentiment(filepath, row_num):
     sent = list()
     array = []
@@ -33,18 +67,21 @@ def sentiment(filepath, row_num):
     df = pd.DataFrame(sent, columns = ['polarity','subjectivity'])
     include =['object', 'float', 'int'] 
     desc = df.describe(include = include)
-    print(df.mean(axis=0))
+    print(desc)
+    #print(df.mean(axis=0))
     # print(df.describe().mean)
     # print(desc.mean(axis=0))
-    # arr = np.array(sent)
-    # print(np.argwhere(np.isclose(arr, 0.8000, 0.1)))
-    # print(array[20])
-    # print(array[42])
+    #arr = np.array(sent)
+    #print(df.min(axis=0)[0])
+    #print(np.argwhere(np.isclose(arr, df.min(axis=0), 0.1)))
+    #print(np.argmin(arr[1:200], axis=0))
+    #print(arr[15], array[15][1])
+    #print(arr[36], array[36][1])
     # print(array[69])
     # print(array[91])
-    # print(array[104])
+    #print(array[104])
     # print(array[284])
-    # print(array[289])
+    #print(array[289])
     # print(array[303])
     # print(array[314])
 
@@ -65,12 +102,12 @@ def sentiment(filepath, row_num):
 
     # print(scipy.stats.ttest_ind(sent, sent2, equal_var=False))
 
-#sentiment('data/scraping_data/defense2.csv', 0)
-#sentiment('data/scraping_data/intercept-data.csv', 6)
-#sentiment('data/scraping_data/vox-data.csv', 7)
-#sentiment('data/scraping_data/brown_cs.csv', 0)
-#sentiment('data/scraping_data/dep_def.csv', 0)
-#sentiment('data/scraping_data/cv_papers.csv', 2)
-#sentiment('data/scraping_data/ml_papers.csv', 1)
-#sentiment('data/scraping_data/crypto_papers.csv', 1)
-sentiment('data/scraping_data/angel_all.csv', 0)
+sentiment('src/data/scraping_data/ycombo.csv', 0)
+#sentiment('src/data/scraping_data/intercept-data.csv', 6)
+sentiment('src/data/scraping_data/vox-data.csv', 7)
+#sentiment('src/data/scraping_data/brown_cs.csv', 0)
+#sentiment('src/data/scraping_data/dep_def.csv', 0)
+#sentiment('src/data/scraping_data/cv_papers.csv', 2)
+#sentiment('src/data/scraping_data/ml_papers.csv', 1)
+#sentiment('src/data/scraping_data/crypto_papers.csv', 1)
+# sentiment('src/data/scraping_data/angel_all.csv', 0)
